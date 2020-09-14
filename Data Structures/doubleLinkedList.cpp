804c9a3b720d4a53bfc245100e16fe7769e1c013
Nada Medhat Mohamed Ibrahim <nada.medhat@outlook.com>
2020-09-14 23:07:06 +0200

    Update the Double Linked List
    More structured classes.
    Added List Class GetValue().
    Delete Node at any place in the list.
    Added exceptions.

--- Data Structures/doubleLinkedList.cpp
+++ Data Structures/doubleLinkedList.cpp
@@ -2,100 +2,132 @@
 using namespace std;
 
 class Node {
-public:
-	Node* prev; 
+
+	Node* prev;
 	Node* next;
 	int data;
 
-	Node() :prev(NULL), next(NULL), data(0) {}
-	Node(int val) : prev(NULL), next(NULL), data(val) {}
+public:
+	Node(int val, Node* previous = NULL, Node* next = NULL) {
+		this->data = val;
+		this->next = next;
+		this->prev = previous;
+	}
+
+	Node* GetNext() { return next; }
+	Node* GetPrevious() { return prev; }
+	int GetValue() { return data; }
+	void SetNext(Node* node) { this->next = node; }
+	void SetPRevious(Node* node) { this->prev = node; }
 };
 
 class dLlist {
-	Node* head; 
-
-	int getListLength() {
-		if (head != 0) {
-			Node* temp = head;
-			int nodes = 0;
-			while (temp != NULL) {
-				nodes++;
-				temp = temp->next;
-			}
-			return nodes;
-		}
-		return 0;
-	}
+	Node* head;
+	Node* tail;
+	int size;
 
 public:
-	dLlist() : head(NULL) {}
 
-	void insertNode(int val) {
-		Node* newnode = new Node(val); 
+	dLlist() {
+		head = NULL;
+		tail = NULL;
+		size = 0;
+	}
 
-		if (head == 0) {
-			head = newnode; 
-		}
-		else {
-			Node* tailptr = head;
-			while (tailptr->next != NULL) {
-				tailptr = tailptr->next; 
-			}
-			tailptr->next = newnode; 
-			newnode->prev = tailptr;
+	~dLlist() {
+		for (int i = 0; i < size; i++) {
+			Node* temp = head;
+			head = temp->GetNext();
+			delete temp;
 		}
 	}
 
-	void deleteNodebyindex(int idx) {
-		
-		if (idx < 0)
-			return;
-		if (idx > getListLength() - 1)
-			return;
-		
-		if (idx == 0) {
-			if (head != 0) { // the list is not empty
-				Node* temp = head; 
-				head = temp->next; 
+	void AppendNode(int val) {
+		Node* newnode = new Node(val);
 
-				delete temp; 
-				if(head!= 0) head->prev = NULL;
-
-			}
+		if (!head) {
+			head = tail = newnode;
 		}
 		else {
-			int cnt = 0; 
-			Node* temp = head; 
-			Node* prevnode = NULL;
-			while (cnt != idx && temp->next != NULL) {
-				prevnode = temp; 
-				temp = temp->next; 
-				cnt++;
-			}
-			if (temp->next == NULL) { // delete from last
-				prevnode->next = NULL; 
-				delete temp;
-			}
-			else{
-				prevnode->next = temp->next; 
-				temp->next->prev = prevnode; 
-				delete temp; 
-			}
+			tail->SetNext(newnode);
+			newnode->SetPRevious(tail);
+			tail = newnode;
 		}
+		size++;
 	}
 
-	void displayList() {
+	void DeleteNode(int index) {
+
+		if (!head || index < 0 || index > size - 1)
+		{
+			throw exception();
+		}
+
+		if (index == 0) {
+
+			Node* temp = head;
+			temp->SetNext(head);
+			delete temp;
+
+			if (head) head->SetPRevious(NULL);
+
+		}
+		else {
+			int currentindex = 0;
+			Node* currentnode = head;
+			Node* previousnode = NULL;
+
+			while (currentindex < index && currentnode->GetNext() != NULL) {
+				previousnode = currentnode;
+				currentnode = currentnode->GetNext();
+				currentindex++;
+			}
+
+			if (currentnode->GetNext() == NULL) { // Delete from last
+				previousnode->SetNext(NULL);
+				tail = previousnode; // Don't forget to update the tail pointer
+
+				delete currentnode;
+			}
+			else {
+
+				previousnode->SetNext(currentnode->GetNext());
+				currentnode->GetNext()->SetPRevious(previousnode);
+
+				delete currentnode;
+			}
+		}
+		size--;
+	}
+
+	void DisplayList() {
 		if (head != 0) { // the list is not empty
-			Node* temp = head; 
-			for (int i = 0; i < getListLength(); i++) {
-				cout << temp->data << " "; 
-				temp = temp->next;
+			for (int i = 0; i < size; i++) {
+				cout << GetValue(i) << " ";
 			}
-			cout << endl; 
+			cout << endl;
 		}
 		else {
 			cout << "List is empty" << endl;
 		}
+	}
+
+	int GetValue(int index)
+	{
+		int currentIndex = 0;
+		Node* node = head;
+		if (!head || index < 0 || index > size)
+		{
+			throw exception();
+		}
+
+		while (currentIndex < index)
+		{
+			node = node->GetNext();
+			currentIndex++;
+		}
+
+		return node->GetValue();
 	}
 };
 
@@ -103,32 +135,32 @@ public:
 int main() {
 
 	dLlist myList;
-	myList.insertNode(1);
-	myList.insertNode(2);
-	myList.insertNode(3);
-	myList.insertNode(4);
-	myList.insertNode(5);
-	
-	myList.displayList(); 
 
-	myList.deleteNodebyindex(4); 
+	myList.AppendNode(1);
+	myList.AppendNode(2);
+	myList.AppendNode(3);
+	myList.AppendNode(4);
+	myList.AppendNode(5);
 
-	myList.displayList();
+	myList.DisplayList();
 
-	myList.insertNode(6);
-	myList.insertNode(7);
+	myList.DeleteNode(4);
 
-	myList.displayList();
+	myList.DisplayList();
 
-	myList.deleteNodebyindex(4);
+	myList.AppendNode(6);
+	myList.AppendNode(7);
 
-	myList.displayList();
+	myList.DisplayList();
 
-	myList.deleteNodebyindex(4);
+	myList.DeleteNode(4);
 
-	myList.displayList();
+	myList.DisplayList();
 
+	myList.DeleteNode(4);
 
-	system("pause"); 
-	return 0; 
+	myList.DisplayList();
+
+	system("pause");
+	return 0;
 }
